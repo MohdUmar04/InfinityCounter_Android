ff81830a082eb6ea8b519ea4a393d4269fe223e6
Mohd. Umar <mohdumarsiddiqui04@gmail.com>
2024-04-19 12:32:47 +0530

    V2

--- app/src/main/java/com/example/counter/Splash.java
+++ app/src/main/java/com/example/counter/Splash.java
@@ -2,9 +2,15 @@ package com.example.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
@@ -13,13 +19,20 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Intent home=new Intent(Splash.this, MainActivity.class);
+
+//        Animation fadeIn = new AlphaAnimation(1,0);
+//        fadeIn.setInterpolator(new DecelerateInterpolator());
+//        fadeIn.setDuration(5000);
+//        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView logo = findViewById(R.id.logoTransparent);
+
+//        logo.startAnimation(fadeIn);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(home);
                 finish();
             }
-        },3000);
+        },2000);
 
     }
 }
